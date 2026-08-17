09366d0a630c6fd78c1919d746c68f469c496b9d
Emanuel Henkel <emanuel.henkel@gmail.com>
2026-08-17 17:41:09 -0300

    feat: add PAT authentication to API routes

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,21 @@ use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/vehicles', [VehicleController::class, 'findAll']);
+    Route::get('/vehicles', [VehicleController::class, 'findAll']);
 
-Route::prefix('vehicle')->group(function () {
+    Route::prefix('vehicle')->group(function () {
 
-    Route::post('/', [VehicleController::class, 'store']);
-    Route::get('/', [VehicleController::class, 'findAll']);
-    Route::get('/{id}', [VehicleController::class, 'findOne']);
-    Route::put('/{id}', [VehicleController::class, 'update']);
-    Route::patch('/{id}', [VehicleController::class, 'patch']);
-    Route::delete('/{id}', [VehicleController::class, 'remove']);
+        Route::post('/', [VehicleController::class, 'store']);
+        Route::get('/', [VehicleController::class, 'findAll']);
+        Route::get('/{id}', [VehicleController::class, 'findOne']);
+        Route::put('/{id}', [VehicleController::class, 'update']);
+        Route::patch('/{id}', [VehicleController::class, 'patch']);
+        Route::delete('/{id}', [VehicleController::class, 'remove']);
 
+    });
 });
